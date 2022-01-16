0786627bef8ac99a3f4b1d47fcf93692a48c0411
kimia <farshadniakimia@gmail.com>
2022-01-17 00:06:49 +0330

    this is first commit

---
+++ js/scipt.js
@@ -0,0 +1,9 @@
+debugger;
+
+function show() {
+  document.getElementById("subtitle").style.display = "block";
+}
+
+function hidden() {
+  document.getElementById("subtitle").style.display = "none";
+}
