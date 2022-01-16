1228fdf4443577d9c8e6bbc5e0956d3ddac4fbe3
kimia farshadnia <76030017+kimiafr@users.noreply.github.com>
2022-01-17 00:08:33 +0330

    Delete scipt.js
    this file not important and has a problem

--- js/scipt.js
+++
@@ -1,9 +0,0 @@
-debugger;
-
-function show() {
-  document.getElementById("subtitle").style.display = "block";
-}
-
-function hidden() {
-  document.getElementById("subtitle").style.display = "none";
-}
